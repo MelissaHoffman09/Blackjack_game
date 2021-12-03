702ab19ffca71c7a48872b8a7359caec63059ada
MelissaHoffman09 <melisssahoffman09@gmail.com>
2021-12-02 22:05:25 -0800

    #

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Melissa",
+    chips: 200
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,17 +10,19 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    let randomNumer = Math.floor( Math.random()*13 ) + 1
-    if (randomNumer === 1 ) {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10 ) {
+        return 10
+    }
+    else if ( randomNumber === 1) {
         return 11
     }
-    else if ( randomNumer >= 11) {
-        return 10 
-    }
     else {
-        return randomNumer
+        return randomNumber
     }
 }
 
